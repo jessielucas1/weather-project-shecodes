634fceeccafce38ceaf0baf24dcb2023fe661f65
jessielucas1 <113065595+jessielucas1@users.noreply.github.com>
2023-01-11 16:17:49 -0500

    Added all files

---
+++ src/script.js
@@ -0,0 +1,62 @@
+function showDate() {
+  let now = new Date();
+
+  let h3 = document.querySelector("h3");
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  h3.innerHTML = `${day} ${hours} : ${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+}
+showDate();
+
+function city(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-search");
+  let cityName = document.querySelector("h1");
+  cityName.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", city);
+
+function searchCity(city) {
+  let apiKey = "53f3bc1f5d348c44be3e3754c7185573";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function showTemperature(response) {
+  let cityElement = document.querySelector("h1");
+  let temperatureElement = document.querySelector("h2");
+  let humidityElement = document.querySelector(".humid");
+  let windElement = document.querySelector(".wind");
+  let feelslikeElement = document.querySelector(".feelsLike");
+  let sunriseElement = document.querySelector(".sunriseTime");
+  let sunsetElement = document.querySelector(".sunsetTime");
+  sunriseElement.innerHTML = `${response.data.sys.sunrise} AM`;
+  sunsetElement.innerHTML = `${response.data.sys.sunset} PM`;
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)} °C`;
+  humidityElement.innerHTML = `${response.data.main.humidity} %`;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)} mph`;
+  feelslikeElement.innerHTML = `${Math.round(
+    response.data.main.feels_like
+  )} °C`;
+}
